d1770caed92046d926a4e0f4b524fbf083ae80cd
Rabe3 <a.rabe3.425@gmail.com>
2025-10-29 00:00:52 +0300

    ..

--- frontend/functions/api/[[path]].js
+++ frontend/functions/api/[[path]].js
@@ -1,4 +1,4 @@
-export async function onRequest(context: any) {
+export async function onRequest(context) {
   const url = new URL(context.request.url);
   
   const workerUrl = 'https://agents-starter.c28b89ed17355a8554949df804a3d70e.workers.dev' + url.pathname + url.search;
