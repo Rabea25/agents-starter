0876f370193ad3c743776dd43ff40a3c2aedcbf6
Rabe3 <a.rabe3.425@gmail.com>
2025-10-28 23:54:22 +0300

    connect to frontend

---
+++ frontend/functions/OnRequest.ts
@@ -0,0 +1,11 @@
+export async function onRequest(context: any) {
+  const url = new URL(context.request.url);
+  
+  const workerUrl = 'https://agents-starter.c28b89ed17355a8554949df804a3d70e.workers.dev' + url.pathname + url.search;
+  
+  return fetch(workerUrl, {
+    method: context.request.method,
+    headers: context.request.headers,
+    body: context.request.body
+  });
+}
